8473817d9f62829c1b9fdf3ea90031205a130edb
Alejandro <a.chataing.a@gmail.com>
2020-12-19 16:39:53 -0400

    New commit

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 require( 'dotenv' ).config();
-const express = require( 'express' );
 const app = express();
-const port = process.env.PORT || 3000;
-const { parse } = require( 'discord-command-parser' );
-const Discord = require( 'discord.js' );
 const client = new Discord.Client();
-const { Client } = require('pg')
+const { Client } = require( 'pg' );
 const connectionString = process.env.DATABASE_URL
-const database = new Client( {
-  connectionString,
-  ssl: { rejectUnauthorized: false }
-} );
 const commands = {
   'config': ( message, parsed ) => {
     const options = {
@@ -40,7 +32,23 @@ const commands = {
     return typeof author !='undefined' && author.id != '789723522770927617' ? console.log( 'default' ) : console.log( 'same bot' );
   }
 }
-var servers;
+const database = new Client( {
+  connectionString,
+  ssl: { rejectUnauthorized: false }
+} );
+const Discord = require( 'discord.js' );
+const express = require( 'express' );
+const { parse } = require( 'discord-command-parser' );
+const port = process.env.PORT || 3000;
+let servers;
+
+const embedMessage = ( type = 'success', title = '', description ) => {
+  return new Discord.MessageEmbed()
+    .setColor( type === 'success' ? '#0099ff' : ( type === 'danger' ? '#F93A2F' : ( type === 'warning' ? '#CC7900' : '#969C9F' ) ) )
+    .setTitle( title )
+    .setDescription( description )
+    .setTimestamp();
+}
 
 const makeQuery = async ( query ) => {
   return await new Promise( async ( resolve, reject ) => {
@@ -54,14 +62,6 @@ const makeQuery = async ( query ) => {
   } );
 }
 
-const embedMessage = ( type = 'success', title = '', description ) => {
-  return new Discord.MessageEmbed()
-    .setColor( type === 'success' ? '#0099ff' : ( type === 'danger' ? '#F93A2F' : ( type === 'warning' ? '#CC7900' : '#969C9F' ) ) )
-    .setTitle( title )
-    .setDescription( description )
-    .setTimestamp();
-}
-
 client.on( "ready", async () => {
   console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
   client.user.setActivity(`on ${client.guilds.size} servers`);
@@ -72,8 +72,7 @@ client.on( "ready", async () => {
 client.on( 'guildCreate', async guild => {
   let foundServer = servers.rows.find( row => row.serv_discord_id === guild.id );
   if ( typeof foundServer == 'undefined' ) {
-    let result = await makeQuery( `INSERT INTO servers ( serv_discord_id, serv_name, serv_prefix ) VALUES ( ${guild.id}, '${guild.name}', '$' )` );
-    console.log( result );
+    await makeQuery( `INSERT INTO servers ( serv_discord_id, serv_name, serv_prefix ) VALUES ( ${guild.id}, '${guild.name}', '$' )` );
   }
 } );
 
@@ -83,8 +82,6 @@ client.on( 'message', message => {
   return ( parsed.success && ( commands[ parsed.command ] || commands[ 'default' ] )( message, parsed ) );
 } );
 
-client.login( process.env.BOT_TOKEN );
-
 app.listen( port, '0.0.0.0', () => {
   console.log( 'Listening on Port ' + port );
 } );
@@ -92,3 +89,5 @@ app.listen( port, '0.0.0.0', () => {
 app.get( '/', ( req, res ) => {
   res.send( 'hello world' );
 } );
+
+client.login( process.env.BOT_TOKEN );
